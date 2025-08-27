89be623ca19403d06fa418fc8187341b97d65fcb
JoaoVitorGirardii <joao.vitor.girardii@gmail.com>
2025-08-26 22:23:42 -0300

    feat: add do Dockerfile e docker-compose

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:22
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm i
+
+COPY . .
+
+COPY .env .
+
+RUN npm run build 
+
+EXPOSE 3000
+
+CMD [ "npm", "run", "start:prod" ]
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AUTH_SERVICE, USUARIO_SERVICE } from './const/services.const';
 
+const AUTH_SERVICE_HOST = process.env.AUTH_SERVICE_HOST || '127.0.0.1';
+const AUTH_SERVICE_PORT = process.env.AUTH_SERVICE_PORT || 3001;
+
+const USUARIO_SERVICE_HOST = process.env.USUARIO_SERVICE_HOST || '127.0.0.1';
+const USUARIO_SERVICE_PORT = process.env.USUARIO_SERVICE_PORT || 3001;
+
 @Module({
   imports: [
     ClientsModule.register([
@@ -11,16 +17,16 @@ import { AUTH_SERVICE, USUARIO_SERVICE } from './const/services.const';
         name: AUTH_SERVICE,
         transport: Transport.TCP,
         options: {
-          host: '127.0.0.1',
-          port: 3001,
+          host: AUTH_SERVICE_HOST,
+          port: AUTH_SERVICE_PORT as number,
         },
       },
       {
         name: USUARIO_SERVICE,
         transport: Transport.TCP,
         options: {
-          host: '127.0.0.1',
-          port: 3002,
+          host: USUARIO_SERVICE_HOST,
+          port: USUARIO_SERVICE_PORT as number,
         },
       },
     ]),
